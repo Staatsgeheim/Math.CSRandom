1e6c7819e66ee65d3cbb434b4c2354f175a4ab49
Staatsgeheim <maarten.boone@gmail.com>
2015-12-23 18:08:51 +0100

    Remove debug output

--- Math.CSRandom.js
+++ Math.CSRandom.js
@@ -5,6 +5,6 @@
 // @version     1
 // @grant       none
 
-unsafeWindow.eval("function CSRandom() { var csPRN = new Uint32Array(2);	window.crypto.getRandomValues(csPRN);	var finalRN = ((csPRN[0] * Math.pow(2,20)) + (csPRN[1] >>> 8)) * Math.pow(2,-56); console.log(finalRN); return finalRN; }; Math.random = CSRandom;");
+unsafeWindow.eval("function CSRandom() { var csPRN = new Uint32Array(2);	window.crypto.getRandomValues(csPRN);	var finalRN = ((csPRN[0] * Math.pow(2,20)) + (csPRN[1] >>> 8)) * Math.pow(2,-56); /* console.log(finalRN); */ return finalRN; }; Math.random = CSRandom;");
 
 // ==/UserScript==
